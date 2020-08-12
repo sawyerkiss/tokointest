9db5c813d8bed6bcbcb1b4455393e9bf3ed6a4b6
Macintosh HD <>
2020-08-12 10:44:30 +0700

    First init

---
+++ tokointest/tokointest/NewsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  tokointest
+//
+//  Created by Macintosh HD on 8/12/20.
+//  Copyright © 2020 Macintosh HD. All rights reserved.
+//
+
+import UIKit
+
+class NewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ tokointest/tokointest/TopHeadlineViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  tokointest
+//
+//  Created by Macintosh HD on 8/12/20.
+//  Copyright © 2020 Macintosh HD. All rights reserved.
+//
+
+import UIKit
+
+class TopHeadlineViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
